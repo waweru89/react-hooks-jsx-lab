20bd1f1f04946f316769f9a2f32c1c74029c69db
MBURU <wawerumburu157@gmail.com>
2024-11-17 14:28:30 +0300

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { image } from "../data/data";
-
+import { image } from "../data/data"; 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I made this portfolio using React!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
